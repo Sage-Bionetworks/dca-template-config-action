cefc71a3c7283fef4654324de208d8195702cab4
afwillia <anthony.williams@sagebase.org>
2023-11-10 13:47:31 -0800

    fix filename and pwd

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -l
 
+pwd
 cd /srv/shiny-server/app
-Rscript -e "install.packages(c('dplyr', 'httr'))"
-file=$(Rscript -e 'source("R/template-config.R"); write_dca_template_config("$1", "$2")')
+Rscript --vanilla -e "install.packages(c('dplyr', 'httr'))"
+file=$(Rscript --vanilla -e 'source("R/template_config.R"); write_dca_template_config("$1", "$2")')
 cat "file=$file" >> $GITHUB_OUTPUT
