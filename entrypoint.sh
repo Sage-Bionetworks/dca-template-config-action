44d0cbcb74c3ed47116876b751b4bb812cf12683
afwillia <anthony.williams@sagebase.org>
2023-11-10 13:27:24 -0800

    try setting workdir to app

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -l
 
-Rscript -e 'renv::restore("/srv/shiny-server/app")'
 Rscript -e 'install.packages("/srv/shiny-server/app", repos = NULL, type = "source")'
 file=$(Rscript -e 'datacurator::write_dca_template_config("$1", "$2")')
 cat "file=$file" >> $GITHUB_OUTPUT
