5e34e4ddf502575761ad205ec1576673f3af1e76
afwillia <anthony.williams@sagebase.org>
2023-11-10 13:52:53 -0800

    don't use environ file

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 pwd
 cd /srv/shiny-server/app
-Rscript --vanilla -e "install.packages(c('dplyr', 'httr'))"
+Rscript --no-environ -e "install.packages(c('dplyr', 'httr'))"
 file=$(Rscript --vanilla -e 'source("R/template_config.R"); source("R/schematic_rest_api.R"); write_dca_template_config("$1", "$2")')
 cat "file=$file" >> $GITHUB_OUTPUT
