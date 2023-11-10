375131431ba1534a0f98b80d0b33c248350739cd
afwillia <anthony.williams@sagebase.org>
2023-11-10 13:50:18 -0800

    source rest api script

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 pwd
 cd /srv/shiny-server/app
 Rscript --vanilla -e "install.packages(c('dplyr', 'httr'))"
-file=$(Rscript --vanilla -e 'source("R/template_config.R"); write_dca_template_config("$1", "$2")')
+file=$(Rscript --vanilla -e 'source("R/template_config.R"); source("R/schematic_rest_api.R"); write_dca_template_config("$1", "$2")')
 cat "file=$file" >> $GITHUB_OUTPUT
