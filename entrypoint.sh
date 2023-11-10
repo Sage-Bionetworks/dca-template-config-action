a14baec1aa7475fb3ba423bf4ccb68a90672cf4d
afwillia <anthony.williams@sagebase.org>
2023-11-10 12:49:36 -0800

    install datacurator package

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 
-tmpfile=$(mktemp /tmp/config.XXXXXX)
-Rscript -e 'datacurator::write_dca_template_config("$1", "$2")' >> $tmpfile
-cat "file=$tmpfile" >> $GITHUB_OUTPUT
+Rscript -e 'devtools::install_local()'
+file=$(Rscript -e 'datacurator::write_dca_template_config("$1", "$2")')
+cat "file=$file" >> $GITHUB_OUTPUT
