f5f9ddef488e2bafc9503888ff57c0000db745f4
afwillia <anthony.williams@sagebase.org>
2023-11-10 12:57:28 -0800

    use base install.packages

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 
-Rscript -e 'devtools::install_local()'
+Rscript -e 'install.packages(".", repos = NULL)'
 file=$(Rscript -e 'datacurator::write_dca_template_config("$1", "$2")')
 cat "file=$file" >> $GITHUB_OUTPUT
