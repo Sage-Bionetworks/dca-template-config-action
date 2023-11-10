ac45ace289b46f15bcdb9b66dfa288e20d880b1b
afwillia <anthony.williams@sagebase.org>
2023-11-10 13:39:24 -0800

    install httr and dplyr to be safe

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 
 cd /srv/shiny-server/app
+Rscript -e "install.packages(c('dplyr', 'httr'))"
 file=$(Rscript -e 'source("R/template-config.R"); write_dca_template_config("$1", "$2")')
 cat "file=$file" >> $GITHUB_OUTPUT
