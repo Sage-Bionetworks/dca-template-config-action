29ec085df69cb56cb975a2fe0e4cb26ea68580a3
afwillia <anthony.williams@sagebase.org>
2023-11-10 13:13:49 -0800

    try adding type = source to install.packages

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 
-Rscript -e 'install.packages("../app", repos = NULL)'
+Rscript -e 'install.packages("./", repos = NULL, type = "source")'
 file=$(Rscript -e 'datacurator::write_dca_template_config("$1", "$2")')
 cat "file=$file" >> $GITHUB_OUTPUT
