28fe681243bbc3e5183e91518928478419a8efd2
afwillia <anthony.williams@sagebase.org>
2023-11-10 13:38:40 -0800

    renv dirs are a mess, just source the file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -l
 
 cd /srv/shiny-server/app
-Rscript -e 'install.packages("./", repos = NULL, type = "source")'
-file=$(Rscript -e 'datacurator::write_dca_template_config("$1", "$2")')
+file=$(Rscript -e 'source("R/template-config.R"); write_dca_template_config("$1", "$2")')
 cat "file=$file" >> $GITHUB_OUTPUT
