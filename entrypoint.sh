2a7e487d3c47ebbc0728e3a668b33fafc9fd3ea5
afwillia <anthony.williams@sagebase.org>
2023-11-13 12:52:23 -0800

    try running as root

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/sh -l
 
-file=$(Rscript -e ".libPaths('/home/shiny/.cache/R/renv/library/app-fec508d6/R-4.1/x86_64-pc-linux-gnu'); datacurator::create_dca_template_config(Sys.getenv('data_model'))")
-echo "$file"
-echo "file=$file" >> "$GITHUB_OUTPUT"
+sudo Rscript -e ".libPaths('/home/shiny/.cache/R/renv/library/app-fec508d6/R-4.1/x86_64-pc-linux-gnu'); datacurator::write_dca_template_config(Sys.getenv('data_model'), Sys.getenv('path'))"
