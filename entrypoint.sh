54df8f13950b5562551d393a9a1d64e0561d3544
afwillia <anthony.williams@sagebase.org>
2023-11-13 14:14:50 -0800

    test adding to github_output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -l
 
 CMD=".libPaths('/home/shiny/.cache/R/renv/library/app-fec508d6/R-4.1/x86_64-pc-linux-gnu'); datacurator::create_dca_template_config(Sys.getenv('data_model'))"
-echo "file=$(Rscript -e $CMD)" >> $GITHUB_OUTPUT
+echo "file=$(Rscript -e $CMD)"
+echo $file
+echo "file=test" >> $GITHUB_OUTPUT
