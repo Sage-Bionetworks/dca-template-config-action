5dfbc7d0014ce4f6af3b40a06ee0bb8d7bd4bdea
afwillia <anthony.williams@sagebase.org>
2023-11-13 12:48:13 -0800

    quote github env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -l
 
 file=$(Rscript -e ".libPaths('/home/shiny/.cache/R/renv/library/app-fec508d6/R-4.1/x86_64-pc-linux-gnu'); datacurator::create_dca_template_config(Sys.getenv('data_model'))")
-echo "file=$file" >> $GITHUB_OUTPUT
+echo "$file"
+echo "file=$file" >> "$GITHUB_OUTPUT"
