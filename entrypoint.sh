11b39d97bda7d78f2c29d48fa5f5c24538d16739
afwillia <anthony.williams@sagebase.org>
2023-11-13 11:13:23 -0800

    use double quotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-Rscript -e "message(list(a=\"$1\", b=\"$2\")); install.packages(c(\"remotes\", \"dplyr\", \"httr\")); remotes::install_local(\"/srv/shiny-server/app\"); datacurator::write_dca_template_config(\"$1\", \"$2\")"
+Rscript -e "message(list(a="$1", b="$2")); install.packages(c("remotes", "dplyr", "httr")); remotes::install_local("/srv/shiny-server/app"); datacurator::write_dca_template_config("$1", "$2")"
