48f74664a63e03ea6e3f3e4c93caa95e6a9aaea9
afwillia <anthony.williams@sagebase.org>
2023-11-13 10:11:34 -0800

    don't run R in vanilla

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
-file=$(Rscript --vanilla -e 'datacurator::write_dca_template_config("$1", "$2")')
+file=$(Rscript -e 'datacurator::write_dca_template_config("$1", "$2")')
 cat "file=$file" >> $GITHUB_OUTPUT
