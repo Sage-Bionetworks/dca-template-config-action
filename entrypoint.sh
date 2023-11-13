457eb430e16c293a56c519f30350a94a88592484
afwillia <anthony.williams@sagebase.org>
2023-11-13 15:42:38 -0800

    add test file

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,3 +2,5 @@
 
 pwd
 Rscript -e ".libPaths('/home/shiny/.cache/R/renv/library/app-fec508d6/R-4.1/x86_64-pc-linux-gnu'); datacurator::create_dca_template_config(Sys.getenv('data_model'))"
+file=test
+echo "file=$file" >> $GITHUB_OUTPUT
