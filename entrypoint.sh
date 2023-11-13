db983b34c081885b7fd8cec73f576932b10fb2cf
afwillia <anthony.williams@sagebase.org>
2023-11-13 12:05:43 -0800

    make data-model data_model

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-Rscript -e "message(list(a=Sys.getenv("data_model"), b=Sys.getenv("path"))); install.packages(c("remotes", "dplyr", "httr")); remotes::install_local("/srv/shiny-server/app"); datacurator::write_dca_template_config(Sys.getenv("data-model"), Sys.getenv("path"))"
+Rscript -e "message(list(a=Sys.getenv('data_model'), b=Sys.getenv('path'))); install.packages(c("remotes", "dplyr", "httr")); remotes::install_local("/srv/shiny-server/app"); datacurator::write_dca_template_config(Sys.getenv('data_model'), Sys.getenv('path'))"
