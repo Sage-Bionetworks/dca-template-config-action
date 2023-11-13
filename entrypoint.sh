449afd0c899d9943e6057ca436dcc23b488b76d7
afwillia <anthony.williams@sagebase.org>
2023-11-13 10:23:51 -0800

    use renv in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
-file=$(Rscript -e 'datacurator::write_dca_template_config("$1", "$2")')
+file=$(Rscript -e 'renv::restore(); datacurator::write_dca_template_config("$1", "$2")')
 cat "file=$file" >> $GITHUB_OUTPUT
