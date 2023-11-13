8e9a631bfdb1142b0eb7baf7d8977d404805d5d1
afwillia <anthony.williams@sagebase.org>
2023-11-13 12:22:08 -0800

    test manually setting libPaths

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-Rscript -e "install.packages(c('remotes', 'dplyr', 'httr')); remotes::install_local('/srv/shiny-server/app'); datacurator::create_dca_template_config(Sys.getenv('data_model'))"
+Rscript -e ".libPaths('/home/shiny/.cache/R/renv/library/app-fec508d6/R-4.1/x86_64-pc-linux-gnu'); datacurator::create_dca_template_config(Sys.getenv('data_model'))"
