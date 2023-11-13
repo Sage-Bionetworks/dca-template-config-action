4f4cce5cc6824a9e1d662a552994d07632fd4520
afwillia <anthony.williams@sagebase.org>
2023-11-13 12:56:56 -0800

    sudo doesn't work

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -l
 
-sudo Rscript -e ".libPaths('/home/shiny/.cache/R/renv/library/app-fec508d6/R-4.1/x86_64-pc-linux-gnu'); datacurator::write_dca_template_config(Sys.getenv('data_model'), Sys.getenv('path'))"
+file=$(Rscript -e ".libPaths('/home/shiny/.cache/R/renv/library/app-fec508d6/R-4.1/x86_64-pc-linux-gnu'); datacurator::write_dca_template_config(Sys.getenv('data_model'), Sys.getenv('path'))")
+echo "file=$file" >> "$GITHUB_OUTPUT"
