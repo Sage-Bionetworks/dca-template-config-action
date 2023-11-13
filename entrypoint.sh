ba2cf61f4dac389bf1870da13b9e666e0238fb53
afwillia <anthony.williams@sagebase.org>
2023-11-13 10:55:30 -0800

    print args

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
-file=$(Rscript -e "list('a'='$1', 'b'='$2'); install.packages(c('remotes', 'dplyr', 'httr')); remotes::install_local('/srv/shiny-server/app'); datacurator::write_dca_template_config('$1', '$2')")
+file=$(Rscript -e "message(list('a'='$1', 'b'='$2')); install.packages(c('remotes', 'dplyr', 'httr')); remotes::install_local('/srv/shiny-server/app'); datacurator::write_dca_template_config('$1', '$2')")
 cat "file=$file" >> $GITHUB_OUTPUT
