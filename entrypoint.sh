1eb4fe53690e2d631d6628426ba61cf0b22ca8bf
afwillia <anthony.williams@sagebase.org>
2023-11-13 10:07:43 -0800

    run as shiny user for renv

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/sh -l
 
-pwd
-cd /srv/shiny-server/app
-Rscript --no-environ -e "install.packages(c('dplyr', 'httr'))"
-file=$(Rscript --vanilla -e 'source("R/template_config.R"); source("R/schematic_rest_api.R"); write_dca_template_config("$1", "$2")')
+file=$(Rscript --vanilla -e 'write_dca_template_config("$1", "$2")')
 cat "file=$file" >> $GITHUB_OUTPUT
