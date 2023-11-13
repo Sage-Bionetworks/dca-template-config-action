7e7f7c2e88e90b34d6eeb97907798a65e95e5d57
afwillia <anthony.williams@sagebase.org>
2023-11-13 10:42:53 -0800

    don't change dir and manually install pacakge

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh -l
 
-ls
-cd /srv/shiny-server/app
-file=$(Rscript -e 'renv::restore(); datacurator::write_dca_template_config("$1", "$2")')
+file=$(Rscript -e 'install.packages(c("remotes", "dplyr", "httr")); remotes::install_local("/srv/shiny-server/app"); datacurator::write_dca_template_config("$1", "$2")')
 cat "file=$file" >> $GITHUB_OUTPUT
