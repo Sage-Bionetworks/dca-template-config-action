12de2d39dfbc718c04407ab75713e7f4e7223741
afwillia <anthony.williams@sagebase.org>
2023-11-13 14:01:08 -0800

    echo output to github_output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/sh -l
 
-file=$(Rscript -e ".libPaths('/home/shiny/.cache/R/renv/library/app-fec508d6/R-4.1/x86_64-pc-linux-gnu'); datacurator::write_dca_template_config(Sys.getenv('data_model'), Sys.getenv('path'))")
-echo "file=$file" >> "$GITHUB_OUTPUT"
+echo "file=$(Rscript -e \".libPaths('/home/shiny/.cache/R/renv/library/app-fec508d6/R-4.1/x86_64-pc-linux-gnu'); datacurator::create_dca_template_config(Sys.getenv('data_model'))\"" >> "$GITHUB_OUTPUT"
