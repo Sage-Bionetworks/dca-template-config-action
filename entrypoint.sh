fe01975303c51f455f06b7d7ba89b5d4bbd67fa2
afwillia <anthony.williams@sagebase.org>
2023-11-13 15:49:17 -0800

    write json file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,3 @@
 #!/bin/sh -l
 
-pwd
-Rscript -e ".libPaths('/home/shiny/.cache/R/renv/library/app-fec508d6/R-4.1/x86_64-pc-linux-gnu'); datacurator::create_dca_template_config(Sys.getenv('data_model'))"
-file=test
-echo "file=$file" >> $GITHUB_OUTPUT
+Rscript -e ".libPaths('/home/shiny/.cache/R/renv/library/app-fec508d6/R-4.1/x86_64-pc-linux-gnu'); datacurator::write_dca_template_config(Sys.getenv('data_model'), Sys.getenv('path')"
