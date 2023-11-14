6ff3abce123dc509799d1483122eb9aa4ec34d73
afwillia <anthony.williams@sagebase.org>
2023-11-13 16:14:32 -0800

    add include and exclude schemas

--- script.R
+++ script.R
@@ -7,4 +7,11 @@ if (path == "") stop("Need to supply path env variable")
 # Set path for renv library within dca container
 .libPaths("/home/shiny/.cache/R/renv/library/app-fec508d6/R-4.1/x86_64-pc-linux-gnu")
 
-datacurator::write_dca_template_config(dm, path)
+# include_schemas and exclude_schemas can't both be non NULL
+if (Sys.getenv("include_schemas") == "") include_schemas <- NULL
+if (Sys.getenv("exclude_schemas") == "") exclude_schemas <- NULL
+
+datacurator::write_dca_template_config(data_model = dm,
+                                       path = path,
+                                       include_schemas = include_schemas,
+                                       exclude_schemas = exclude_schemas)
