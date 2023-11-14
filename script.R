4d442a41f26351eb408cf9f0c76b242f4dbb6e9a
afwillia <anthony.williams@sagebase.org>
2023-11-13 16:17:59 -0800

    use file instead of path for consistency

--- script.R
+++ script.R
@@ -1,8 +1,8 @@
 dm <- Sys.getenv("data_model")
 if (dm == "") stop("Need to supply data_model env variable")
 
-path <- Sys.getenv("path")
-if (path == "") stop("Need to supply path env variable")
+file <- Sys.getenv("file")
+if (file == "") stop("Need to supply path env variable")
 
 # Set path for renv library within dca container
 .libPaths("/home/shiny/.cache/R/renv/library/app-fec508d6/R-4.1/x86_64-pc-linux-gnu")
@@ -12,6 +12,6 @@ if (Sys.getenv("include_schemas") == "") include_schemas <- NULL
 if (Sys.getenv("exclude_schemas") == "") exclude_schemas <- NULL
 
 datacurator::write_dca_template_config(data_model = dm,
-                                       path = path,
+                                       file = file,
                                        include_schemas = include_schemas,
                                        exclude_schemas = exclude_schemas)
