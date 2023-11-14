b13f0e345bea11df7a102a095a7afef16c78c5eb
afwillia <anthony.williams@sagebase.org>
2023-11-13 16:54:01 -0800

    process include and exclude strings

--- script.R
+++ script.R
@@ -8,8 +8,19 @@ if (file == "") stop("Need to supply `file` env variable")
 .libPaths("/home/shiny/.cache/R/renv/library/app-fec508d6/R-4.1/x86_64-pc-linux-gnu")
 
 # include_schemas and exclude_schemas can't both be non NULL
-if (Sys.getenv("include_schemas") == "") include_schemas <- NULL
-if (Sys.getenv("exclude_schemas") == "") exclude_schemas <- NULL
+include_schemas <- Sys.getenv("include_schemas")
+ifelse(include_schemas == "") include_schemas <- NULL
+if (!is.null(include_schemas)) {
+  include_schemas <- strsplit(include_schemas, " ")
+  include_schemas <- include_schemas[[1]]
+}
+
+exclude_schemas <- Sys.getenv("exclude_schemas")
+if (exclude_schemas == "") exclude_schemas <- NULL
+if (!is.null(exclude_schemas)) {
+  exclude_schemas <- strsplit(exclude_schemas, " ")
+  exclude_schemas <- exclude_schemas[[1]]
+}
 
 datacurator::write_dca_template_config(data_model = dm,
                                        file = file,
