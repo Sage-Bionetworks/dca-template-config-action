f4becc170475777dec11e672a459cc71f8b737da
afwillia <anthony.williams@sagebase.org>
2023-11-13 16:24:00 -0800

    make var filename

--- script.R
+++ script.R
@@ -1,8 +1,8 @@
 dm <- Sys.getenv("data_model")
-if (dm == "") stop("Need to supply data_model env variable")
+if (dm == "") stop("Need to supply `data_model` env variable")
 
-file <- Sys.getenv("file")
-if (file == "") stop("Need to supply path env variable")
+filename <- Sys.getenv("file")
+if (filename == "") stop("Need to supply `filename` env variable")
 
 # Set path for renv library within dca container
 .libPaths("/home/shiny/.cache/R/renv/library/app-fec508d6/R-4.1/x86_64-pc-linux-gnu")
@@ -12,6 +12,6 @@ if (Sys.getenv("include_schemas") == "") include_schemas <- NULL
 if (Sys.getenv("exclude_schemas") == "") exclude_schemas <- NULL
 
 datacurator::write_dca_template_config(data_model = dm,
-                                       file = file,
+                                       file = filename,
                                        include_schemas = include_schemas,
                                        exclude_schemas = exclude_schemas)
